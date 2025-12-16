73353cfbe153cb9f3f81b0807e340fd7187bf7ed
CaroliCarol <a59626@alunos.ipb.pt>
2025-12-16 01:07:14 +0000

    1. NavMesh Movement

--- Assets/Scripts/RobberBehaviour.cs
+++ Assets/Scripts/RobberBehaviour.cs
@@ -12,6 +12,9 @@ public class RobberBehaviour : MonoBehaviour
     public GameObject van;
     NavMeshAgent agent;
 
+    public enum ActionState { IDLE, WORKING };
+    ActionState state = ActionState.IDLE;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -33,14 +36,33 @@ public class RobberBehaviour : MonoBehaviour
 
     public Node.Status GoToDiamond()
     {
-        agent.SetDestination(diamond.transform.position);
-        return Node.Status.SUCESS;
+        return GoToLocation(diamond.transform.position);
     }
 
     public Node.Status GoToVan()
     {
-        agent.SetDestination(van.transform.position);
-        return Node.Status.SUCESS;
+        return GoToLocation(van.transform.position);
+    }
+
+    Node.Status GoToLocation(Vector3 destination)
+    {
+        float distanceToTarget = Vector3.Distance(destination, this.transform.position);
+        if (state == ActionState.IDLE)
+        {
+            agent.SetDestination(destination);
+            state = ActionState.WORKING;
+        }
+        else if (Vector3.Distance(agent.pathEndPosition, destination) >= 2)
+        {
+            state = ActionState.IDLE;
+            return Node.Status.FAILURE;
+        }
+        else if (distanceToTarget < 2)
+        {
+            state = ActionState.IDLE;
+            return Node.Status.SUCESS;
+        }
+        return Node.Status.RUNNING;
     }
 
     // Update is called once per frame
